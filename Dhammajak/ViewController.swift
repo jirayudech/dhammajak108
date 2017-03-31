fdf6fe7d331440e7f3822830189f7d19be591c67
MacBookPro <mac@pwin-mac-mini.local>
2017-03-31 09:31:20 +0700

    Initial Commit

---
+++ Dhammajak/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dhammajak
+//
+//  Created by Mac mini on 3/31/2560 BE.
+//  Copyright © 2560 RGT Soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
